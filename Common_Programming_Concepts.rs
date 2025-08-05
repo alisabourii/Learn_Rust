351f6409dacd9265938107588ebbed7df19c5ffe
Ali SABOURI <72344723+alisabourii@users.noreply.github.com>
2025-08-05 23:19:33 +0300

    Update Common_Programming_Concepts.rs

--- Common_Programming_Concepts.rs
+++ Common_Programming_Concepts.rs
@@ -6,11 +6,15 @@ fn main() {
 
     const THREE_HOURS_IN_SECONDS: u32 = 60*60*3;*/
     
-    let x = 5;
+    /*let x = 5;
     let x = x+1;
     {
         let x = x*2;
         println!("The of X in the inner scope is:{x}")
     }
-    println!("The value of x is: {x}");
+    println!("The value of x is: {x}");*/
+
+    let spaces = "1 3";
+    let spaces = spaces.len();
+    println!("The value of xspaces is: {spaces}");
 }
