97f5a7ecf201082daf8a60e3952b52d202243040
Ali SABOURI <72344723+alisabourii@users.noreply.github.com>
2025-08-05 18:30:59 +0300

    Update Common_Programming_Concepts.rs

--- Common_Programming_Concepts.rs
+++ Common_Programming_Concepts.rs
@@ -1,6 +1,16 @@
 fn main() {
-    let mut x = 5;
+    /*let mut x = 5;
     println!("The Value of X: {x}");
     x = 6;
-    print!("The Value of x: {x}")
+    print!("The Value of x: {x}");
+
+    const THREE_HOURS_IN_SECONDS: u32 = 60*60*3;*/
+    
+    let x = 5;
+    let x = x+1;
+    {
+        let x = x*2;
+        println!("The of X in the inner scope is:{x}")
+    }
+    println!("The value of x is: {x}");
 }
