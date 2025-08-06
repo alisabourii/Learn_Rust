99557d74dd96ddba6a4efaa2580c6e1ba68d3027
Ali SABOURI <72344723+alisabourii@users.noreply.github.com>
2025-08-06 14:34:32 +0300

    Update Common_Programming_Concepts.rs

--- Common_Programming_Concepts.rs
+++ Common_Programming_Concepts.rs
@@ -1,4 +1,5 @@
 fn main() {
+    //-------- Variables and Mutability -----------
     /*let mut x = 5;
     println!("The Value of X: {x}");
     x = 6;
@@ -14,7 +15,34 @@ fn main() {
     }
     println!("The value of x is: {x}");*/
 
+    /*
     let spaces = "1 3";
     let spaces = spaces.len();
-    println!("The value of xspaces is: {spaces}");
+    println!("The value of xspaces is: {spaces}");*/
+
+
+    //------------- Data Types -----------
+    
+    //let guess = "42".parse().expect("Not a numeric value");
+
+    /*let x=2.0;
+    let y=3.0;
+    println!("The value of X is: {x}");
+    println!("The value of Y is: {y}");*/
+
+    //Addition
+    let sum = 5+10;
+
+    //Subtraction
+    let diffrent = 95.5-4.3;
+
+    //multiplication
+    let product = 4*30;
+
+    //division
+    let quotient = 56.7/32.2;
+    let truncated = -5/3;
+
+    //Reminder
+    let remaindier = 43%3; 
 }
