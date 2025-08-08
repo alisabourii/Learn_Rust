31a80e97ad12cc93c00aa239b99b91b3e99e017f
Ali <72344723+alisabourii@users.noreply.github.com>
2025-08-08 12:37:04 +0300

    Yeni güncellemeler

--- programming_concepts/src/main.rs
+++ programming_concepts/src/main.rs
@@ -1,4 +1,7 @@
+use std::io;
+
 fn main() {
+    //-------- Variables and Mutability -----------
     /*let mut x = 5;
     println!("The Value of X: {x}");
     x = 6;
@@ -14,7 +17,100 @@ fn main() {
     }
     println!("The value of x is: {x}");*/
 
+    /*
     let spaces = "1 3";
     let spaces = spaces.len();
-    println!("The value of xspaces is: {spaces}");
+    println!("The value of xspaces is: {spaces}");*/
+
+
+    //------------- Data Types -----------
+    
+    //let guess = "42".parse().expect("Not a numeric value");
+
+    /*let x=2.0;
+    let y=3.0;
+    println!("The value of X is: {x}");
+    println!("The value of Y is: {y}");*/
+
+    //Addition
+    /*let sum = 5+10;
+
+    //Subtraction
+    let diffrent = 95.5-4.3;
+
+    //multiplication
+    let product = 4*30;
+
+    //division
+    let quotient = 56.7/32.2;
+    let truncated = -5/3;
+
+    //Reminder
+    let remaindier = 43%3; */
+
+    //------- The Boolean Type -------
+    let t=true;
+
+    let f=false;
+
+    //-------- The Tuple Type--------
+    let tup = (500,6.4,1);
+    let (x,y,z) = tup;
+    //println!("The Value of y is: {y}");
+
+    //-------- The Array Type--------
+    let a = [1,2,3,4,5];
+
+    let months = ["January", "February", "March", "April", "May", "June", "July",
+              "August", "September", "October", "November", "December"];
+
+    let a = [3; 5];
+
+    let firstMonth = months[0];
+    let lastMonth = months[11];
+    //println!("First Month : {firstMonth}");
+    //println!("Last Month : {lastMonth}");
+
+    //foundArrayIndex();
+    //another_func(5);
+    //print_labeled_measurement(5, 'H');
+    if_flow();
+}
+
+fn foundArrayIndex(){
+    let a = [1,2,3,4,5];
+    println!("Enter Your Array Index: ");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line!!");
+    
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index enterd was not a number");
+
+    let element = a[index];
+
+    println!("The value of the element at index {index} is: {element}")
+
+}
+
+fn another_func(x:i32){
+    print!("The Value of X: {x}");
+}
+
+fn print_labeled_measurement(value:i32, unit_label:char){
+    println!("The measurement is: {value}{unit_label}");
+
+}
+fn if_flow(){
+    let number = 8;
+    if number < 5{
+        println!("Condition was true!");
+    }else {
+        println!("Condition was false!");
+    }
 }
