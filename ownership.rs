dcbacca52a61519cd9bd88d9c3714eaea4a15c62
Ali SABOURI <72344723+alisabourii@users.noreply.github.com>
2025-08-10 13:21:36 +0300

    Update ownership.rs

--- ownership.rs
+++ ownership.rs
@@ -36,3 +36,14 @@ fn no_dangle()->String{
     let s = String::from("Hello");
     s
 }
+
+fn first_word(s:&String) -> usize{
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+    s.len()
+}
